809637b6c919484e035c78cb5558e0db04d8c8e1
Your Name <you@example.com>
2024-08-08 15:17:55 +0000

    exo 3 + sort.h update + 3-O

--- sort.h
+++ sort.h
@@ -23,10 +23,11 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+
 void quick_sort(int *array, size_t size);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
-int partition(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
+int partition(int *array, int low, int high, size_t size);
 void quick_sort_rec(int *array, int low, int high, size_t size);
 
 void print_array(const int *array, size_t size);
