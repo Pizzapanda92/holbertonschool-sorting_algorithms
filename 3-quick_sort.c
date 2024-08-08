d5fe35fc4204bc007d109fb0db022a72b61726ce
Your Name <you@example.com>
2024-08-08 14:55:00 +0000

    exo 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,79 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order
+ *              using the Quick sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort_rec(array, 0, size - 1, size);
+}
+
+/**
+ * quick_sort_rec - Recursive function to perform Quick sort
+ * @array: array to be sorted
+ * @low: leftmost index of the subarray
+ * @high: rightmost index of the subarray
+ * @size: full size of the array
+ */
+void quick_sort_rec(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pivot_index = partition(array, low, high, size);
+		quick_sort_rec(array, low, pivot_index - 1, size);
+		quick_sort_rec(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
+ * partition - Partitions the array using Lomuto partition scheme
+ * @array: array to be partitioned
+ * @low: leftmost index of the subarray
+ * @high: rightmost index of the subarray
+ * @size: full size of the array
+ * Return: pivot index
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+
+	for (int j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+
+	if (array[i + 1] != array[high])
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+
+	return i + 1;
+}
+
+/**
+ * swap - Swaps two integers in an array
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
